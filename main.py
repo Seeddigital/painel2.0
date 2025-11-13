f18bf63e0be61d1009efe80dfc2e83da29267049
Seeddigital <31134011+Seeddigital@users.noreply.github.com>
2025-11-13 12:24:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
         "https://chamados-dev-seed.web.app",
         "https://f1caee16-06fd-4a62-877f-325cc7fad0eb.lovableproject.com",
         "https://preview--painel-seed.lovable.app/clients",
-        "https://painel.seeddigital.com.br/clients",# ← Adicione esta linha
+        "https://painel.seeddigital.com.br",# ← Adicione esta linha
     ],
     allow_credentials=True,
     allow_methods=["*"],
