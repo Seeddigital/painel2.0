d193d7b51d41add6cc46ef02479e4242aa910d00
Seeddigital <31134011+Seeddigital@users.noreply.github.com>
2025-11-28 13:08:17 -0300

    Update gaps_integracao.py

--- queries/gaps_integracao.py
+++ queries/gaps_integracao.py
@@ -11,21 +11,9 @@ def get_gaps_integracao(conn):
         [FLUXO],
         [TICKET],
         [CHAVE]
-    FROM [Seed_CFG_Analytics].[dbo].[DS_INTEGRACAO_GAP]
+    FROM [Seed_CFG_Analytics].[dbo].[DS_INTEGRACAO_GAP_FULL]
+    WHERE [TICKET] IS NULL
     ORDER BY [DATA] DESC, [HORA] ASC
     """
-    cursor = conn.cursor()
-    cursor.execute(query)
-    columns = [column[0] for column in cursor.description]
-    results = []
+    ...
 
-    for row in cursor.fetchall():
-        row = dict(zip(columns, row))
-
-        # acrescentar transformações opcionais
-        row["STATUS_COLOR"] = "red" if row["TICKET"] == "GAP" else "green"
-        row["STATUS_EMOJI"] = "🔴" if row["TICKET"] == "GAP" else "🟢"
-
-        results.append(row)
-
-    return results
