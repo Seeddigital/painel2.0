f0e25fcab13791d58957da9ae761b5805603afb7
Seeddigital <31134011+Seeddigital@users.noreply.github.com>
2025-12-02 10:11:31 -0300

    Update company.py

--- queries/company.py
+++ queries/company.py
@@ -1,1 +1,21 @@
+def get_company(conn):
+    query = """
+    SELECT
+          [DS_COMPANY_DESCRIPTION]
+        , [DS_COMPANY_EMPRESA_ID]
+        , [DS_STATUS]
+        , [DS_COMPANY_SENHA_INTEGRACAO]
+    FROM [Seed_CFG_Analytics].[dbo].[DS_COMPANY]
+    """
+    
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    # Captura dinamicamente os nomes das colunas
+    columns = [column[0] for column in cursor.description]
+
+    # Zipa cada linha com suas colunas → vira dicionário
+    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    return results
 
