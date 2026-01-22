29cf98c11fb0a1f0347124561cba14b8a804186e
Seeddigital <31134011+Seeddigital@users.noreply.github.com>
2026-01-22 16:36:26 -0300

    Update consulta_sensores_desinstalados.py

--- queries/consulta_sensores_desinstalados.py
+++ queries/consulta_sensores_desinstalados.py
@@ -3,38 +3,46 @@ def get_sensores_desinstalados(conn):
 
     query = """
     WITH SensoresPorChamado AS (
-        SELECT 
-            C.DS_COMPANY_DESCRIPTION AS CLIENTE,
-            B.DS_SITE_DESCRIPTION AS LOJA,
-            CAST(A.DS_CHAMADO_DT_AGENDAMENTO AS DATE) AS DATA,
-            value AS SENSOR_ID
-        FROM 
-            [Seed_CFG_Analytics].[dbo].[DS_CHAMADOS] A
-        INNER JOIN 
-            [Seed_CFG_Analytics].[dbo].[DS_SITE] B 
-            ON A.DS_CHAMADO_SITE_ID = B.DS_SITE_BS_ID
-        INNER JOIN 
-            [Seed_CFG_Analytics].[dbo].[DS_COMPANY] C 
-            ON B.DS_SITE_COMPANY_ID_BS = C.DS_COMPANY_BS_ID
-        CROSS APPLY 
-            STRING_SPLIT(A.DS_CHAMADO_SENSOR_ID, ',')
-        WHERE 
-            A.DS_CHAMADO_TIPO = 'D'
-            AND A.DS_CHAMADO_SENSOR_ID IS NOT NULL
-    )
-    SELECT 
-        CLIENTE,
-        LOJA,
-        DATA,
-        COUNT(*) AS TOTAL_SENSORES_DESINSTALADOS
+    SELECT DISTINCT
+        C.DS_COMPANY_DESCRIPTION AS CLIENTE,
+        B.DS_SITE_DESCRIPTION AS LOJA,
+        CAST(A.DS_CHAMADO_DT_AGENDAMENTO AS DATE) AS DATA,
+        LTRIM(RTRIM(S.value)) AS SENSOR_ID
     FROM 
-        SensoresPorChamado
-    GROUP BY 
-        CLIENTE,
-        LOJA,
-        DATA
-    ORDER BY 
-        DATA DESC
+        [Seed_CFG_Analytics].[dbo].[DS_CHAMADOS] A
+    INNER JOIN 
+        [Seed_CFG_Analytics].[dbo].[DS_SITE] B 
+        ON A.DS_CHAMADO_SITE_ID = B.DS_SITE_BS_ID
+    INNER JOIN 
+        [Seed_CFG_Analytics].[dbo].[DS_COMPANY] C 
+        ON B.DS_SITE_COMPANY_ID_BS = C.DS_COMPANY_BS_ID
+    CROSS APPLY 
+        STRING_SPLIT(A.DS_CHAMADO_SENSOR_ID, ',') S
+    WHERE 
+        A.DS_CHAMADO_TIPO = 'D'
+        AND A.DS_CHAMADO_SENSOR_ID IS NOT NULL
+        AND EXISTS (
+            SELECT 1
+            FROM [Seed_CFG_Analytics].[dbo].[DS_DATA_SOURCE] D
+            WHERE D.DS_DATA_SOURCE_SENSOR_ID = LTRIM(RTRIM(S.value))
+              AND D.DS_DATA_SOURCE_TM_ID NOT IN (7,8,13,14,16,20,21)
+        )
+)
+
+SELECT 
+    CLIENTE,
+    LOJA,
+    DATA,
+    COUNT(*) AS TOTAL_SENSORES_DESINSTALADOS
+FROM 
+    SensoresPorChamado
+GROUP BY 
+    CLIENTE,
+    LOJA,
+    DATA
+ORDER BY 
+    DATA DESC;
+
     """
 
     cursor.execute(query)
